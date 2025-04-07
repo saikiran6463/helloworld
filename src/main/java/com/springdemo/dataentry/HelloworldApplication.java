7ef5035fbae38902a37e0a139e4787df69894932
Sai Kiran <saikiran6463@gmail.com>
2025-04-06 23:25:50 -0700

    Cloned and committed : Added comment to Main class

--- src/main/java/com/springdemo/dataentry/HelloworldApplication.java
+++ src/main/java/com/springdemo/dataentry/HelloworldApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloworldApplication {
-
+//Here is where the main application starts.
     public static void main(String[] args)
     {
         SpringApplication.run(HelloworldApplication.class, args);
